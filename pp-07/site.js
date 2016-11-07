6f42efd4d6d8419272ffd381f13d3b639635e61f
Theo Economos <theoeconomos1@gmail.com>
2016-11-07 15:59:31 -0600

    second problem done

--- pp-07/site.js
+++ pp-07/site.js
@@ -21,3 +21,12 @@ console.log('The value of x is:', x, 'It should be 5.');
   variables nor the double function can be accessed via the
   `window` global object, like `window.x`).
 */
+
+function double(num) {
+  var x = 5;
+  x = num * 2;
+  return x;
+}
+
+double(6);
+console.log('The value of x is:', x, 'It should be 5.');
